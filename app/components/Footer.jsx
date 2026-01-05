dad88c8785059fb5dd22b7970d85a91133e6b097
Arshpreet Kaur <arshpreetashu43@gmail.com>
2026-01-05 16:15:10 +0530

    update code

---
+++ app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="bg-slate-900">
+      <p className="text-center text-white py-2">
+        &copy;2026 Arshpreet Kaur | All Rights Reserved
+      </p>
+    </div>
+  )
+}
+
+export default Footer
+
--- app/sections/Home.jsx
+++ app/sections/Home.jsx
@@ -4,6 +4,8 @@ import Spaceman from "../components/Spaceman";
 import { Canvas } from "@react-three/fiber";
 import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import { Suspense, useEffect, useState } from "react";
+import TextPressure from '../components/TextPressure';
+import BlurText from '../components/BlurText';
 
 import Image from "next/image";
 const Background= "/images/background.webp"
@@ -44,13 +46,34 @@ export default function HeroSection() {
         transition={{ duration: 0.7 }}
         className="flex flex-col gap-4 max-w-xl text-center md:text-left"
       >
-        <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-tight text-gray-100">
+        {/* <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold leading-tight text-gray-100">
           Hi, I'm <span className="text-blue-500">Arshpreet Kaur</span>
-        </h1>
-
+        </h1> */}
+  <div className="relative w-full max-w-xl">
+  <TextPressure
+   text={`Hi, I'm\n\nArshpreet Kaur`}
+    flex
+    width
+    weight
+    italic
+    textColor="#ffffff"
+    strokeColor="#ff0000"
+    minFontSize={46}
+  />
+</div>
+{/* 
         <p className="text-gray-300 text-base sm:text-lg lg:text-xl">
           Frontend & MERN Stack Developer creating clean UI, smooth UX, and modern web applications.
-        </p>
+        </p> */}
+        <motion.p
+  initial={{ opacity: 0, y: 20 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ delay: 0.5, duration: 0.8 }}
+  className="text-gray-300 text-base sm:text-lg lg:text-xl tracking-wide leading-relaxed max-w-lg mt-6"
+>
+  Frontend & MERN Stack Developer creating clean UI, smooth UX, and modern web applications.
+</motion.p>
+
       </motion.div>
 
       {/* RIGHT 3D MODEL SECTION */}
